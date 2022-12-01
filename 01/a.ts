c30f3b2013487a284cc127ba46cae35c18c080d5
Horacio <horacio@horaciovalencia.com>
2022-11-30 22:47:26 -0700

    day 1

--- 01/a.ts
+++ 01/a.ts
@@ -1,6 +1,20 @@
 import * as p from "https://deno.land/std@0.165.0/path/mod.ts";
 const input = await Deno.readTextFile(
-  p.fromFileUrl(import.meta.resolve("./input.txt")),
+  p.fromFileUrl(import.meta.resolve("./input.txt"))
 );
 
-console.log(input);
+const elves = input.split(/\r?\n/) as string[];
+
+let cur = 0;
+let max = 0;
+for (const cal of elves) {
+  if (!cal) {
+    max = cur > max ? cur : max;
+    cur = 0;
+  } else {
+    cur += parseInt(cal);
+  }
+}
+max = cur > max ? cur : max;
+
+console.log(max);
--- 01/b.ts
+++ 01/b.ts
@@ -1,6 +1,24 @@
 import * as p from "https://deno.land/std@0.165.0/path/mod.ts";
 const input = await Deno.readTextFile(
-  p.fromFileUrl(import.meta.resolve("./input.txt")),
+  p.fromFileUrl(import.meta.resolve("./input.txt"))
 );
 
-console.log(input);
+const elves = input.split(/\r?\n/) as string[];
+
+let cur = 0;
+let cals: number[] = [];
+for (const cal of elves) {
+  if (!cal) {
+    cals.push(cur);
+    cur = 0;
+  } else {
+    cur += parseInt(cal);
+  }
+}
+
+console.log(
+  cals
+    .sort((a, b) => a - b)
+    .slice(-3)
+    .reduce((total, n) => total + n, 0)
+);
