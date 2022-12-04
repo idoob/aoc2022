23d4aa1d8fbd83f972ef8e9c70714e064b4311cd
Horacio <horacio@horaciovalencia.com>
2022-12-03 22:46:57 -0700

    Choked on part b because i cant read

---
+++ 04/a.ts
@@ -0,0 +1,29 @@
+import * as p from "https://deno.land/std@0.165.0/path/mod.ts";
+// @deno-types="https://cdn.skypack.dev/@types/lodash?dts"
+import { range } from "https://cdn.skypack.dev/lodash-es";
+
+const input = await Deno.readTextFile(
+  p.fromFileUrl(import.meta.resolve("./input.txt"))
+);
+
+const lines = input.split(/\s+/);
+
+let count = 0;
+lines.map((line) => {
+  const pair = line.split(",");
+
+  const [l1, l2] = pair[0].split("-");
+  const [r1, r2] = pair[1].split("-");
+
+  const leftSet = new Set(range(parseInt(l1), parseInt(l2) + 1));
+  const rightSet = new Set(range(parseInt(r1), parseInt(r2) + 1));
+
+  const intersection = new Set(
+    [...leftSet].filter((element) => rightSet.has(element))
+  );
+
+  if (intersection.size === leftSet.size || intersection.size === rightSet.size) {
+    count += 1;
+  }
+});
+console.log(count);
